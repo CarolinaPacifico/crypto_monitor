8383b9af33a5c3a373e50d1b07f53e32d8f3dac1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-12 20:14:34 -0300

    Ajustando MainActivity

--- app/src/main/java/com/github/carolinapacifico/crypto_monitor/service/MercadoBitcoinServiceFactory.kt
+++ app/src/main/java/com/github/carolinapacifico/crypto_monitor/service/MercadoBitcoinServiceFactory.kt
@@ -1,7 +1,13 @@
-package com.github.carolinapacifico.crypto_monitor.service
+package com.github.carolinapacifico.crypto_monitor
 
+import com.github.carolinapacifico.crypto_monitor.service.MercadoBitcoinService
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class MercadoBitcoinServiceFactory {
 
@@ -9,8 +15,25 @@ class MercadoBitcoinServiceFactory {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://www.mercadobitcoin.net/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(unsafeOkHttpClient())
             .build()
 
         return retrofit.create(MercadoBitcoinService::class.java)
+    }
+
+    private fun unsafeOkHttpClient(): OkHttpClient {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
     }
 }
